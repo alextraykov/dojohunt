93321a5212cdd7830e9a1c8749341ccaca27518d
alextraykov <alextraykov@gmail.com>
2019-08-07 17:10:26 +0300

    linked axios & foursquares api

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
-import { thisExpression } from "@babel/types";
-import ReactDOM from "react-dom";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 import SearchBar from "./SearchBar";
+import axios from "axios";
 
-export class MapContainer extends Component {
+export class App extends Component {
+  state = {
+    venues: []
+  };
+
+  componentWillMount() {
+    this.getVenues();
+  }
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?";
+    const parameters = {
+      client_id: "WEA5DGFHYPFUZDSRT3PJSZLC0TTZKDOCHJMRE2HQWYECNZMD",
+      client_secret: "NJQQBRJFHK5V54QL1XNVJ1YCIPDCQYV1BC02G5SBPXKGBHTD",
+      query: "martial arts",
+      near: "Sofia",
+      v: "20180323"
+    };
+
+    axios
+      .get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({ venues: response.data.response.groups[0].items });
+      })
+      .catch(error => {
+        console.log("ERROR! " + error);
+      });
+  };
+
   render() {
     return (
       <div className="">
@@ -24,4 +51,4 @@ export class MapContainer extends Component {
 }
 export default GoogleApiWrapper({
   apiKey: "AIzaSyDZFmCp6JqYrar3BxIy4XP5N-RC00Hqw6Q"
-})(MapContainer);
+})(App);
