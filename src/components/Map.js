e32a2234386c52b9962e1f11e888ec1e3fae9349
PepoDyakov <petar.dyakov@icloud.com>
2019-12-28 15:18:08 +0200

    Changes to the map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,9 @@
 import React, { useState } from "react";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
-import { MapController } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import VenueButton from "./VenueButton";
 
 export default function Map(props) {
-    const mapController = new MapController();
     const [viewport, setViewport] = useState({
         latitude: 42.6977,
         longitude: 23.3219,
@@ -14,53 +12,19 @@ export default function Map(props) {
         height: "100vh"
     });
 
-    const testFunction = (e, marker) => {
-        e.preventDefault();
-        setSelectedVenue(marker);
-    };
-
     const { item } = props;
-    const [selectedVenue, setSelectedVenue] = useState(null);
-
     return (
         <div>
             <ReactMapGL
                 {...viewport}
                 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-                controller={mapController}
-                onViewportChange={viewport => {
-                    setViewport(viewport);
-                }}
             >
-                {item.map((item, key) => {
-                    return item.map((marker, key) => {
-                        return (
-                            <Marker
-                                key={key}
-                                latitude={marker.venue.location.lat}
-                                longitude={marker.venue.location.lng}
-                            >
-                                <VenueButton
-                                    onClick={e => testFunction(e, marker)}
-                                />
-                            </Marker>
-                        );
-                    });
-                })}
-                {selectedVenue ? (
-                    <Popup
-                        latitude={selectedVenue.venue.location.lat}
-                        longitude={selectedVenue.venue.location.lng}
-                        onClose={() => {
-                            setSelectedVenue(null);
-                        }}
-                    >
-                        <div>
-                            <h2>{selectedVenue.venue.name}</h2>
-                            <button>View More</button>
-                        </div>
-                    </Popup>
-                ) : null}
+                <Marker
+                    latitude={viewport.latitude}
+                    longitude={viewport.longitude}
+                >
+                    <VenueButton />
+                </Marker>
             </ReactMapGL>
         </div>
     );
