9982e38e6db3e86caf21a6cfc00800d2b60ab94a
PepoDyakov <petar.dyakov@icloud.com>
2019-12-28 15:08:43 +0200

    env changes

--- src/components/App.js
+++ src/components/App.js
@@ -5,110 +5,115 @@ import VenuePage from "../pages/VenuePage";
 const queryStrings = ["martial arts", "boxing", "karate"];
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
+    constructor(props) {
+        super(props);
 
-  //   this.state = {
-  //     latlong: "",
-  //     venues: [],
-  //     venueID: undefined,
-  //     imgUrl: ""
-  //   };
-  // }
+        this.state = {
+            latlong: "",
+            venues: [],
+            venueID: undefined,
+            imgUrl: ""
+        };
+    }
 
-  // componentDidMount = () => {
-  //   this.getGeolocation();
-  //   this.getVenueImage();
-  // };
+    componentDidMount = () => {
+        this.getGeolocation();
+        // this.getVenueImage();
+    };
 
-  // getGeolocation = () => {
-  //   navigator.geolocation.getCurrentPosition(response => {
-  //     const latlong =
-  //       response.coords.latitude + "," + response.coords.longitude;
-  //     this.getVenues(latlong);
-  //   });
-  // };
+    getGeolocation = () => {
+        navigator.geolocation.getCurrentPosition(response => {
+            const latlong =
+                response.coords.latitude + "," + response.coords.longitude;
+            this.getVenues(latlong);
+        });
+    };
 
-  // getURLParams = queryString => {
-  //   return {
-  //     client_id: "WEA5DGFHYPFUZDSRT3PJSZLC0TTZKDOCHJMRE2HQWYECNZMD",
-  //     client_secret: "NJQQBRJFHK5V54QL1XNVJ1YCIPDCQYV1BC02G5SBPXKGBHTD",
-  //     ll: "42.6977, 23.3219",
-  //     query: queryString,
-  //     v: "20180323"
-  //   };
-  // };
+    getURLParams = queryString => {
+        return {
+            client_id: "WEA5DGFHYPFUZDSRT3PJSZLC0TTZKDOCHJMRE2HQWYECNZMD",
+            client_secret: "NJQQBRJFHK5V54QL1XNVJ1YCIPDCQYV1BC02G5SBPXKGBHTD",
+            ll: "42.6977, 23.3219",
+            query: queryString,
+            v: "20180323"
+        };
+    };
 
-  // getImgParams = () => {
-  //   return {
-  //     client_id: "WEA5DGFHYPFUZDSRT3PJSZLC0TTZKDOCHJMRE2HQWYECNZMD",
-  //     client_secret: "NJQQBRJFHK5V54QL1XNVJ1YCIPDCQYV1BC02G5SBPXKGBHTD",
-  //     v: "20180323"
-  //   };
-  // };
+    // getImgParams = () => {
+    //   return {
+    //     client_id: "WEA5DGFHYPFUZDSRT3PJSZLC0TTZKDOCHJMRE2HQWYECNZMD",
+    //     client_secret: "NJQQBRJFHK5V54QL1XNVJ1YCIPDCQYV1BC02G5SBPXKGBHTD",
+    //     v: "20180323"
+    //   };
+    // };
 
-  // getVenueImage = () => {
-  //   const client_id = "WEA5DGFHYPFUZDSRT3PJSZLC0TTZKDOCHJMRE2HQWYECNZMD";
-  //   const client_secret = "NJQQBRJFHK5V54QL1XNVJ1YCIPDCQYV1BC02G5SBPXKGBHTD";
-  //   const v = "20180323";
-  //   const venueID = "4e6e469e45dd293273cdf587";
-  //   const endPoint = `https://api.foursquare.com/v2/venues/${venueID}/photos?client_id=${client_id}&client_secret=${client_secret}&v=${v}`;
-  //   axios
-  //     .get(endPoint)
-  //     .then(response => {
-  //       const { items } = response.data.response.photos;
-  //       let imgUrl = "";
-  //       imgUrl = `${items[0].prefix}${items[0].width}x${items[0].height}${
-  //         items[0].suffix
-  //       }`;
-  //       this.setState({
-  //         imgUrl
-  //       });
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // };
-  // getVenues = latlong => {
-  //   let stateObj = [];
-  //   const endPoint = "https://api.foursquare.com/v2/venues/explore?";
-  //   axios
-  //     .all([
-  //       axios.get(
-  //         endPoint + new URLSearchParams(this.getURLParams(queryStrings[0]))
-  //       ),
-  //       axios.get(
-  //         endPoint + new URLSearchParams(this.getURLParams(queryStrings[1]))
-  //       ),
-  //       axios.get(
-  //         endPoint + new URLSearchParams(this.getURLParams(queryStrings[2]))
-  //       )
-  //     ])
-  //     .then(response => {
-  //       response.forEach(responseElement => {
-  //         stateObj.push(responseElement.data.response.groups[0].items);
-  //       });
-  //       this.setState({
-  //         venues: stateObj,
-  //         venueID: stateObj[0][0].venue.id
-  //       });
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // };
+    // getVenueImage = () => {
+    //   const client_id = "WEA5DGFHYPFUZDSRT3PJSZLC0TTZKDOCHJMRE2HQWYECNZMD";
+    //   const client_secret = "NJQQBRJFHK5V54QL1XNVJ1YCIPDCQYV1BC02G5SBPXKGBHTD";
+    //   const v = "20180323";
+    //   const venueID = "4e6e469e45dd293273cdf587";
+    //   const endPoint = `https://api.foursquare.com/v2/venues/${venueID}/photos?client_id=${client_id}&client_secret=${client_secret}&v=${v}`;
+    //   axios
+    //     .get(endPoint)
+    //     .then(response => {
+    //       const { items } = response.data.response.photos;
+    //       let imgUrl = "";
+    //       imgUrl = `${items[0].prefix}${items[0].width}x${items[0].height}${
+    //         items[0].suffix
+    //       }`;
+    //       this.setState({
+    //         imgUrl
+    //       });
+    //     })
+    //     .catch(error => {
+    //       console.log(error);
+    //     });
+    // };
+    getVenues = latlong => {
+        let stateObj = [];
+        const endPoint = "https://api.foursquare.com/v2/venues/explore?";
+        axios
+            .all([
+                axios.get(
+                    endPoint +
+                        new URLSearchParams(this.getURLParams(queryStrings[0]))
+                ),
+                axios.get(
+                    endPoint +
+                        new URLSearchParams(this.getURLParams(queryStrings[1]))
+                ),
+                axios.get(
+                    endPoint +
+                        new URLSearchParams(this.getURLParams(queryStrings[2]))
+                )
+            ])
+            .then(response => {
+                response.forEach(responseElement => {
+                    stateObj.push(
+                        responseElement.data.response.groups[0].items
+                    );
+                });
+                this.setState({
+                    venues: stateObj,
+                    venueID: stateObj[0][0].venue.id
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
 
-  render() {
-    // console.log(this.state.imgUrl);
-    return (
-      <div>
-        {/* {this.state.imgUrl.length > 0 && ( */}
-        {/* <img src={this.state.imgUrl} alt="test" />
+    render() {
+        console.log(this.state);
+        return (
+            <div>
+                {/* {this.state.imgUrl.length > 0 && ( */}
+                {/* <img src={this.state.imgUrl} alt="test" />
         )} */}
-        {/* <Map item={this.state.venues} /> */}
-        <VenuePage />
-      </div>
-    );
-  }
+                <Map item={this.state.venues} />
+                {/* <VenuePage /> */}
+            </div>
+        );
+    }
 }
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -4,77 +4,71 @@ import { MapController } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import VenueButton from "./VenueButton";
 
-console.log(process.env.REACT_APP_MAPBOX_TOKEN);
-
-const REACT_APP_MAPBOX_TOKEN =
-  "pk.eyJ1IjoicG9sZWNhdHNreSIsImEiOiJjano0NmNsOTgwYTIxM2RwYXB0NjFtMHhrIn0.C7TggPuEGoLMKnY7b6Foww";
-
 export default function Map(props) {
-  const mapController = new MapController();
-  const [viewport, setViewport] = useState({
-    latitude: 42.6977,
-    longitude: 23.3219,
-    zoom: 12,
-    width: "100vw",
-    height: "100vh"
-  });
+    const mapController = new MapController();
+    const [viewport, setViewport] = useState({
+        latitude: 42.6977,
+        longitude: 23.3219,
+        zoom: 12,
+        width: "100vw",
+        height: "100vh"
+    });
 
-  const testFunction = (e, marker) => {
-    e.preventDefault();
-    setSelectedVenue(marker);
-  };
+    const testFunction = (e, marker) => {
+        e.preventDefault();
+        setSelectedVenue(marker);
+    };
 
-  //   Object deconstruction =
-  const { item } = props;
-  const [selectedVenue, setSelectedVenue] = useState(null);
+    const { item } = props;
+    const [selectedVenue, setSelectedVenue] = useState(null);
 
-  if (item.length > 0) {
     return (
-      <div>
-        <ReactMapGL
-          {...viewport}
-          mapboxApiAccessToken={REACT_APP_MAPBOX_TOKEN}
-          controller={mapController}
-          onViewportChange={viewport => {
-            setViewport(viewport);
-          }}
-        >
-          {item.map((item, key) => {
-            return item.map((marker, key) => {
-              return (
-                <Marker
-                  key={key}
-                  latitude={marker.venue.location.lat}
-                  longitude={marker.venue.location.lng}
-                >
-                  <VenueButton onClick={e => testFunction(e, marker)} />
-                </Marker>
-              );
-            });
-          })}
-          {selectedVenue ? (
-            <Popup
-              latitude={selectedVenue.venue.location.lat}
-              longitude={selectedVenue.venue.location.lng}
-              onClose={() => {
-                setSelectedVenue(null);
-              }}
+        <div>
+            <ReactMapGL
+                {...viewport}
+                mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                controller={mapController}
+                onViewportChange={viewport => {
+                    setViewport(viewport);
+                }}
             >
-              <div>
-                <h2>{selectedVenue.venue.name}</h2>
-                <button>View More</button>
-              </div>
-            </Popup>
-          ) : null}
-        </ReactMapGL>
-      </div>
+                {item.map((item, key) => {
+                    return item.map((marker, key) => {
+                        return (
+                            <Marker
+                                key={key}
+                                latitude={marker.venue.location.lat}
+                                longitude={marker.venue.location.lng}
+                            >
+                                <VenueButton
+                                    onClick={e => testFunction(e, marker)}
+                                />
+                            </Marker>
+                        );
+                    });
+                })}
+                {selectedVenue ? (
+                    <Popup
+                        latitude={selectedVenue.venue.location.lat}
+                        longitude={selectedVenue.venue.location.lng}
+                        onClose={() => {
+                            setSelectedVenue(null);
+                        }}
+                    >
+                        <div>
+                            <h2>{selectedVenue.venue.name}</h2>
+                            <button>View More</button>
+                        </div>
+                    </Popup>
+                ) : null}
+            </ReactMapGL>
+        </div>
     );
-  }
-  return (
-    <div className="ui">
-      <div className="ui active inverted dimmer">
-        <div className="ui text loader">Discovering Dojos</div>
-      </div>
-    </div>
-  );
 }
+// return (
+//     <div className="ui">
+//         <div className="ui active inverted dimmer">
+//             <div className="ui text loader">Discovering Dojos</div>
+//         </div>
+//     </div>
+// );
